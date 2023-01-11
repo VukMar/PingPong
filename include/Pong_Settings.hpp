465192b1d6937ddf023a5ba731f1a3e91c965774
VukMar <vuk.s.maric@gmail.com>
2023-01-12 00:28:46 +0100

    Added Human or AI settings for both players.

--- include/Pong_Settings.hpp
+++ include/Pong_Settings.hpp
@@ -6,19 +6,26 @@
 #include "D:/VS Code Vuk/C++/SFMLUI/include/UI_Button.hpp"
 #include "D:/VS Code Vuk/C++/SFMLUI/include/UI_CheckBox.hpp"
 
+namespace Settings
+{
+    enum Player{ One = 0, Two = 1};
+}
+
+
 class SettingsPage
 {
 private:
 
 sf::Font font;
 
-vui::CheckBox AI_opponent;
-vui::CheckBox Player_opponent;
-
 sf::Text Title;
-sf::Text OpponentText;
-sf::Text OpponentText1;
-sf::Text OpponentText2;
+
+vui::CheckBox AI_opponent[2];
+vui::CheckBox Player_opponent[2];
+
+sf::Text OpponentText[2];
+sf::Text OpponentText1[2];
+sf::Text OpponentText2[2];
 
 bool settingsChanged = false;
 
@@ -29,7 +36,11 @@ SettingsPage();
 
 void create(sf::RenderWindow &window);
 
-void Update(bool &HumanOPSelected, bool &AIOPSelected);
+void playerOneSettingsCreate(sf::Vector2f winSize);
+
+void playerTwoSettingsCreate(sf::Vector2f winSize);
+
+void Update(bool &Human_Selected, bool &AI_Selected, Settings::Player player);
 
 bool areSettingsChanged();
 
